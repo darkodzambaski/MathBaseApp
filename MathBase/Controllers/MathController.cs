f6b1c3cda5cc9936593769000b2d54cb9156a887
darkodzambaski <63052308+darkodzambaski@users.noreply.github.com>
2022-08-30 18:15:00 +0200

    Add project files.

---
+++ MathBase/Controllers/MathController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MathBase.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Algebra()
+        {
+            return View();
+        }
+
+        public IActionResult Trigonometry()
+        {
+            return View();
+        }
+
+        public IActionResult Calculus()
+        {
+            return View();
+        }
+
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+    }
+}
---
+++ MathBase/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MathBase.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MathBase.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
