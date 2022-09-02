e5a163a9fecdb48cd73f4a5e08d940cf853912d2
darkodzambaski <63052308+darkodzambaski@users.noreply.github.com>
2022-09-02 18:16:48 +0200

    Home Page edit

---
+++ MathBase/Controllers/QuizController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MathBase.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Quiz()
+        {
+            return View();
+        }
+    }
+}
